136a436bb561a0ffeb938a464915c141700051fd
Imran Akbar <imranakbar@temenos.com>
2020-05-13 01:20:29 +0530

    File Loading issue

--- app.py
+++ app.py
@@ -5,30 +5,33 @@ import os
 
 app = Flask(__name__) 
 
-
-def file_loader():
+#Load states from a text file once the app gets loaded
+lines = ''
+with app.app_context():
     if 'lines' not in g:
         with open('indian_states.txt') as f:
-          g.lines = f.read().splitlines()
-    return g.lines
-
+          lines = f.read().splitlines()
+          
+#Method whenever there is a error in page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error-404.html')
 
+#A tiny page with Stayhome message
 @app.route("/stayhome")
 def stayhome():
     return render_template('stayhome.html') 
 
-
+#Main route for loading the First page, it loads default state first
 @app.route("/")
 def covidHome():
-    list_states = file_loader()
+    list_states = lines
     get_state = request.args.get('state', default='Tamil Nadu')
     covid19 =  covid(get_state,None)
     data = covid19.getStateData()["districtData"]
     return render_template("covidtable.html", Statedata = data, State = get_state, Statelist = list_states)
 
+#Route for chennai related Stats
 @app.route("/covid")
 def covidstat():
     State = "Tamil Nadu"
@@ -37,6 +40,7 @@ def covidstat():
     data = covid19.getChennaiData()
     return render_template("covid19.html", data = data, District=District, State =State)
 
+#Route for State and District related stats passed in URL
 @app.route("/covid/<string:state>/<string:district>")
 def covidstats(state,district):
     State = state.title()
@@ -45,15 +49,17 @@ def covidstats(state,district):
     data = covid19.getChennaiData()
     return render_template("covid19.html", data = data, District=District, State =State)
 
-# New functions
+# About Page
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
+#Contact Page
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
 
+#Sitemap
 @app.route('/sitemap.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
