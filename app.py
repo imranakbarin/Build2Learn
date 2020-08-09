939d45ddcad34f40589d8346f77c7b54194a0d85
Imran Akbar <imranakbar@temenos.com>
2020-08-09 22:06:50 +0530

    Chennai 09

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def newspage():
 @app.route("/chennaicovidlist")
 def chennaistreetwise():
      try:
-        with codecs.open('static/json/chennai_data_08.json','r', 'utf-8-sig') as f:
+        with codecs.open('static/json/chennai_data_09.json','r', 'utf-8-sig') as f:
             chennaidata = json.load(f)
         return render_template('chennaicovidlist.html', chennailist = chennaidata['chennaidata'])      
      except Exception as e:
